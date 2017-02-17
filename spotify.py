667425618f5660012e92b8457a3e8bd9f950c374
BlacKJackal <mcdougal.robert@gmail.com>
2017-02-17 08:35:44 -0600

    Changes to Spotify.py

--- spotify.py
+++ spotify.py
@@ -1,5 +1,3 @@
-# -*- coding: latin-1 -*-
-
 import requests
 import os
 import json
@@ -13,16 +11,18 @@ from optparse import OptionParser
 import time
 import datetime as dt
 import base64
-#import spotipy
-#import spotipy.util as util
 import six
 import six.moves.urllib.parse as urllibparse
 import re
+import unicodedata
+import makeEnglish3
+import string
 
 SPOTIPY_CLIENT_ID ='065d75cdcc7142c8a155166f8466dee9'
 SPOTIPY_CLIENT_SECRET ='2ac84e8c76034a58a6dd4657893faa20'
 
 OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+
 
 def getTrackInfo(mp3file):
 	"""
@@ -36,9 +36,13 @@ def getTrackInfo(mp3file):
 	tags = ID3(mp3file)
 
 	artist = tags.getall("TPE1")[0]
+	albumartist = tags.getall("TPE2")[0]
 	track = tags.getall("TIT2")[0]
 	album = tags.getall("TALB")[0]
-	return artist, track, album
+	if albumartist == 'Various Artists':
+		return artist, track, album
+	else:		
+		return albumartist, track, album
 
 def getISRC(mp3file):
 	tags = ID3(mp3file)
@@ -46,10 +50,6 @@ def getISRC(mp3file):
 	isrc = tags.getall('TXXX:ISRC')
 
 	return str(isrc)
-
-def is_token_expired(token_info):
-	now = int(time.time())
-	return token_info['expires_at'] - now < 60
 
 def _make_authorization_headers(client_id, client_secret):
 	auth_header = base64.b64encode(six.text_type(client_id + ':' + client_secret).encode('ascii'))
@@ -99,18 +99,51 @@ def getSpotifyTrackInfo(trackid):
 def getfirstAlbumID(searchData):
 	return searchData['albums']['items'][0]['id']
 
+def getSpotifyAudioFeatures(spotifyid):
+	audiofeatureURL = "https://api.spotify.com/v1/audio-features/{id}".format(id=spotifyid)
+	headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": authToken}
+	r = requests.get(audiofeatureURL, headers=headers)
+	data = json.loads(r.text)
+	acoustic = (data['acousticness']) * 100
+	energy = (data['energy']) * 100
+	mood = (data['valence']) * 100
+	dance = (data['danceability']) * 100
+	tempo = (data['tempo'])
+	return acoustic, energy, mood, dance, tempo
 
 
-def prependZeros(listeners, playcount):
-	numLisZeros = 10 - len(str(listeners))
-	numPlayZeros = 10 - len(str(playcount))
-	list1 = numLisZeros * "0"
-	play1 = numPlayZeros * "0"
-	list2 = str(list1) + str(listeners)
-	play2 = str(play1) + str(playcount)
-	return list2, play2
 
-def setTags(mp3file):
+def prependZeros(acoustic, energy, mood, dance, tempo):
+	numAcousticZeros = 5 - len(str(acoustic))
+	numEnergyZeros = 5 - len(str(energy))
+	numMoodZeros = 5 - len(str(mood))
+	numDanceZeros = 5 - len(str(dance))
+	numTempoZeros = 5 - len(str(tempo))
+
+	acoustic1 = numAcousticZeros * "0"
+	energy1 = numEnergyZeros * "0"
+	mood1 = numMoodZeros * "0"
+	dance1 = numDanceZeros * "0"
+	tempo1 = numTempoZeros * "0"
+
+	acoustic2 = str(acoustic1) + str(acoustic)
+	energy2 = str(energy1) + str(energy)
+	mood2 = str(mood1) + str(mood)
+	dance2 = str(dance1) + str(dance)
+	tempo2 = str(tempo1) + str(tempo)
+
+	return acoustic2, energy2, mood2, dance2, tempo2
+
+def prependZeroSingle(string):
+	numZeros = 3 - len(str(string))
+
+	string1 = numZeros * "0"
+
+	string2 = str(string1) + str(string)
+
+	return string2
+
+def setSpotifyID(mp3file):
 	try:		
 		if not isTagged(mp3file):
 			audiofile = ID3(mp3file)
@@ -120,9 +153,13 @@ def setTags(mp3file):
 
 			print(track)
 
-			artist = re.sub('[\W_]+', '', artist)
-			track = re.sub('[\W_]+', '', track)
-			album = re.sub('[\W_]+', '', album)
+			# artist = re.sub('[\W_]+', '', artist)
+			# track = re.sub('[\W_]+', '', track)
+			# album = re.sub('[\W_]+', '', album)
+
+			artist = remove_accents(artist)
+			track = remove_accents(track)
+			album = remove_accents(album)
 
 			artist, track, album = str(artist).replace("tacochina", " "), str(track).replace("tacochina", " "), str(album).replace("tacochina", " ")
 
@@ -131,9 +168,10 @@ def setTags(mp3file):
 			artist = urllib.parse.quote_plus(str(artist))
 			track = urllib.parse.quote_plus(str(track))
 			album = urllib.parse.quote_plus(str(album))
-			print(artist + track + album)
 			queryString = "track:{track} artist:{artist}".format(track=track, artist=artist)
+			print(queryString)
 			results = searchspotify(queryString)
+			print(results)
 			spotifyid = results['tracks']['items'][0]['id']
 			audiofile.add(TXXX(encoding=3, desc=u'spotifytrackid', text=str(spotifyid)))
 			audiofile.save()
@@ -145,6 +183,10 @@ def setTags(mp3file):
 	except Exception as e:
 		print("Error occured in setTags with details " + str(e))
 		try:
+			artist, track, album = getTrackInfo(mp3file)
+			with open("errorfiles.txt", "a") as f:
+				f.write("Artist: " + artist + " Track: " + " Album: " + album)
+
 			isrc = getISRC(mp3file)
 			queryString = "isrc:{isrc}".format(isrc=isrc)
 			results = searchspotify(queryString)
@@ -155,6 +197,68 @@ def setTags(mp3file):
 		except Exception as e:
 			print("Failed lookup by ISRC")
 		return 0
+
+def setSpotifyStats(mp3file):
+	try:		
+		if not isTaggedstats(mp3file):
+			audiofile = ID3(mp3file)
+
+			spotifyid = audiofile.getall('TXXX:spotifytrackid')[0]
+
+			spotifyid = urllib.parse.quote_plus(str(spotifyid))
+
+			popularity = prependZeroSingle(getSpotifyTrackInfo(spotifyid)['popularity'])
+
+			print("Popularity: " + popularity)
+
+			acoustic, energy, mood, dance, tempo = getSpotifyAudioFeatures(spotifyid)
+
+			acoustic1, energy1, mood1, dance1, tempo1 = prependZeros(round(acoustic,1), round(energy,1), round(mood,1), round(dance,1), round(tempo,1))
+
+			if not doesTagExist('spotifyenergy', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifyenergy', text=str(energy1)))
+			if not doesTagExist('spotifyacoustic', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifyacoustic', text=str(acoustic1)))
+			if not doesTagExist('spotifymood', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifymood', text=str(mood1)))
+			if not doesTagExist('spotifydance', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifydance', text=str(dance1)))
+			if not doesTagExist('spotifytempo', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifytempo', text=str(tempo1)))
+			if not doesTagExist('spotifypopularity', mp3file):
+				audiofile.add(TXXX(encoding=3, desc=u'spotifypopularity', text=str(popularity)))
+			audiofile.save()
+			return acoustic1, energy1, mood1, dance1, tempo1
+		else:
+			return 0, 0, 0, 0, 0
+	except UnicodeEncodeError as uni:
+		print("Unable to encode some part of the filename " + mp3file + "\n" + "Check for special or foreign characters.")
+	except Exception as e:
+		print("Error occured in setSpotifyStats with details " + str(e))
+		return 0, 0, 0, 0, 0
+
+def isTaggedstats(mp3file):
+	try:
+		tags = ID3(mp3file)
+		trackid = tags.getall('TXXX:spotifypopularity')[0]
+		if trackid is not None:
+			return True
+		else:
+			return False
+	except IndexError as I:
+		return False
+
+def doesTagExist(tagname, mp3file):
+	try:
+		tags = ID3(mp3file)
+		thistag = tags.getall('TXXX:' + tagname)[0]
+		if thistag is not None:
+			return True
+		else:
+			return False
+	except IndexError as I:
+		return False
+
 
 def isTagged(mp3file):
 	try:
@@ -170,8 +274,9 @@ def isTagged(mp3file):
 def clearTags(mp3file):
 	try:
 		audiofile = ID3(mp3file)
-		audiofile.delall('TXXX:lastfmListeners')
-		audiofile.delall('TXXX:lastfmplaycount')
+		audiofile.delall('TXXX:spotifyenergy')
+		audiofile.delall('TXXX:spotifyacoustic')
+		audiofile.delall('TXXX:spotifymood')
 		audiofile.save()
 		print("Removed tags from " + mp3file)
 	except Exception as e:
@@ -247,7 +352,7 @@ def getFileListQuick(dir, age=24):
 		print(str(e))
 		#print("Check Directory string")
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', mp3 = '', spotifyid = ''):
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', mp3 = '', acoustic = '', energy = '', mood = '', dance = '', tempo = ''):
 	"""
 	Call in a loop to create terminal progress bar
 	@params:
@@ -274,7 +379,42 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 	if mp3 != '':
 		print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
 		print(mp3)
-		print("SpotifyID: " + spotifyid)
+		print("Acoustic: " + str(acoustic))
+		print("Energy: " + str(energy))
+		print("Mood: " + str(mood))
+		print("Dance: " + str(dance))
+		print("Tempo: " + str(tempo))
+	if iteration == total: 
+		print()
+
+def printProgressBarID(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', mp3 = '', spotifyid = ''):
+	"""
+	Call in a loop to create terminal progress bar
+	@params:
+		iteration   - Required  : current iteration (Int)
+		total       - Required  : total iterations (Int)
+		prefix      - Optional  : prefix string (Str)
+		suffix      - Optional  : suffix string (Str)
+		decimals    - Optional  : positive number of decimals in percent complete (Int)
+		length      - Optional  : character length of bar (Int)
+		fill        - Optional  : bar fill character (Str)
+		mp3 		- Optional	: mp3 file name (Str)
+		listeners 	- Optional	: number of listeners (Str)
+		playcount 	- Optional	: number of plays (Str)
+
+	@returns:
+		prints a progress bar to the screen unless a MP3 file is being tageged and in that case it will
+		return a progress bar with the file, # of listeners and # of plays
+	"""
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filledLength = int(length * iteration // total)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+	# Print File details if a file is updated.
+	if mp3 != '':
+		print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+		print(mp3)
+		print("SpotifyID: " + str(spotifyid))
 	if iteration == total: 
 		print()
 
@@ -286,16 +426,51 @@ def setTagsProgress(file_list, update_freq = 1):
 	print("Directory Walk Completed, " + str(l) + " files detected, Beginning the tagging process")
 	printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill = 'X')
 	for mp3 in file_list:
-		spotifyid = setTags(mp3)
+		acoustic, energy, mood, dance, tempo = setSpotifyStats(mp3)
 		i += 1
+		if i % 2000 ==0:
+			global authToken
+			authToken = "Bearer " + getSpotifyToken()
 		if i % update_freq == 0:
-			printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill = 'X')
-		if spotifyid != 0:
-			#logging.info('File %s - Adding Listners: %s Adding Playcount: %s', mp3, listeners, playcount)
-			printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill='X', mp3 = mp3, spotifyid = spotifyid)
+			printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill='X', mp3 = mp3, acoustic = acoustic, energy = energy, mood = mood, dance = dance, tempo = tempo)
 		else:
 			#logging.info('File %s - NO CHANGE Listners: %s Playcount: %s', mp3, listeners, playcount)
 			pass
+
+def setIDProgress(file_list, update_freq = 1):
+	i = 0
+	#Total number of files to look at
+	l = len(file_list)
+
+	print("Directory Walk Completed, " + str(l) + " files detected, Beginning the tagging process")
+	printProgressBarID(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill = 'X')
+	for mp3 in file_list:
+		spotifyid = setSpotifyID(mp3)
+		i += 1
+		if i % 2000 ==0:
+			global authToken
+			authToken = "Bearer " + getSpotifyToken()
+		if i % update_freq == 0:
+			printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill = 'X')
+		if spotifyid != 0:
+			printProgressBarID(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50, fill='X', mp3 = mp3, spotifyid = spotifyid)
+		else:
+			#logging.info('File %s - NO CHANGE Listners: %s Playcount: %s', mp3, listeners, playcount)
+			pass
+
+def remove_accents(data):
+	bob = data.replace('+', 'and')
+	bob = bob.replace('-', 'tacochina')
+	bob = ''.join(x for x in unicodedata.normalize('NFD', bob) if x in string.ascii_letters).lower()
+	return bob
+
+def remove_featured_artists(string):
+	artist = string
+	if 'feat' in string:
+		artist, featartist = string.split('feat')
+	if 'with' in string:
+		artist, featartist = string.split('with')
+	return artist
 
 
 def main():
@@ -308,7 +483,7 @@ def main():
 
 	queryString = "track:{track} artist:{artist}".format(track=track, artist=artist)
 	results = searchspotify(queryString)
-	print(results['tracks']['items'][0]['id'])  #results['tracks']['items'][0]['name']
+	print(results['tracks']['items'][0]['id'])
 
 
 
@@ -326,8 +501,11 @@ def main():
 	filelist = getFileList("S:\MusicBee2017\Music")
 	setTagsProgress(filelist)
 
-
-
+	# one, two, three = getSpotifyAudioFeatures("6zPXWXS2pZwSsIM7f08Mg5")
+	# acoustic, energy, mood = prependZeros(round(one,1), round(two,1), round(three,1))
+	# print(acoustic)
+	# print(energy)
+	# print(mood)
 			
 
 if __name__ == "__main__":
