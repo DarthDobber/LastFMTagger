e415637a34d4d4f1ec347ab03313f846e60af12d
DarthDobber <mcdougal.robert@gmail.com>
2017-02-21 16:03:38 -0600

    Adding the Album by Track file

---
+++ spotifyTrackbyAlbum.py
@@ -0,0 +1,15 @@
+import spotify
+
+authToken = "Bearer " + spotify.getSpotifyToken()
+
+def getAlbumID(query):
+	result = spotify.searchSpotify(query,type=album)
+	return result['albums']['items'][0]['id']
+
+def getAlbumTracks(spotifyID):
+	albumTrackURL = "https://api.spotify.com/v1/albums/{id}/tracks".format(id=spotifyID)
+	headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": authToken}
+	r = requests.get(searchTrackURL, headers=headers)
+	data = json.loads(r.text)
+	return data
+
